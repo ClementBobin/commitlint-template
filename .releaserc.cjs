616216cee54d2fe19141c45ba8a46ce86345cbb3
Mirage <119869686+ClementBobin@users.noreply.github.com>
2026-02-18 10:53:50 +0000

    fix(.releaserc.cjs): change file type to allow comment

---
+++ .releaserc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  branches: [
+    { name: "main" }
+  ],
+
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+
+    // enable only ONE depending on platform
+    "@semantic-release/gitlab",
+    // "@semantic-release/azure-devops",
+    // "@semantic-release/bitbucket",
+
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
+};
