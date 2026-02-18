f77efa92a6cc9c6c926649585347647e29e3a44b
Mirage <119869686+ClementBobin@users.noreply.github.com>
2026-02-18 11:36:10 +0000

    test: added back "@semantic-release/github"

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -7,11 +7,16 @@ module.exports = {
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
-
-    // enable only ONE depending on platform
-    //"@semantic-release/gitlab",
-    // "@semantic-release/azure-devops",
-    // "@semantic-release/bitbucket",
+    [
+      // enable only ONE depending on platform
+      // "@semantic-release/gitlab",
+      // "@semantic-release/azure-devops",
+      // "@semantic-release/bitbucket",
+      "@semantic-release/github",
+        {
+          "assets": []
+        }
+    ],
 
     [
       "@semantic-release/git",
